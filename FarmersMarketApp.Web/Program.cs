0561d112739d4aec1c8ffff276d78d86cdd5f9c6
ezopus <133430790+ezopus@users.noreply.github.com>
2024-10-24 23:52:54 +0300

    identity user replaced with application user

--- FarmersMarketApp.Web/Program.cs
+++ FarmersMarketApp.Web/Program.cs
@@ -1,5 +1,5 @@
 using FarmersMarketApp.Infrastructure.Data;
-using Microsoft.AspNetCore.Identity;
+using FarmersMarketApp.Infrastructure.Data.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,7 +10,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddRazorPages();
 
