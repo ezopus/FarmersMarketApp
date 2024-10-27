5b30538dc541c1c1b61e86aa1d998819b3a78077
ezopus <133430790+ezopus@users.noreply.github.com>
2024-10-27 18:18:45 +0200

    application user properties added

--- FarmersMarketApp.Data/Data/Models/ApplicationUser.cs
+++ FarmersMarketApp.Data/Data/Models/ApplicationUser.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using static FarmersMarketApp.Common.DataValidation.ErrorMessages;
+using static FarmersMarketApp.Common.DataValidation.ValidationConstants.ApplicationUserValidation;
 
 namespace FarmersMarketApp.Infrastructure.Data.Models
 {
     public class ApplicationUser : IdentityUser
     {
-        public string FirstName { get; set; } = null!;
+        [Comment("First name of application user.")]
+        [MaxLength(FirstNameMaxLength, ErrorMessage = ErrorUserFirstName)]
+        public required string FirstName { get; set; }
 
-        public string LastName { get; set; } = null!;
+        [Comment("Last name of application user.")]
+        [MaxLength(LastNameMaxLength, ErrorMessage = ErrorUserLastName)]
+        public required string LastName { get; set; }
+
+        [Comment("Role of application user - can be either normal user or admin")]
+        public string? Role { get; set; }
+
+        [Comment("Physical address of application user.")]
+        [MaxLength(AddressMaxLength, ErrorMessage = ErrorUserAddress)]
+        public required string Address { get; set; }
+
+
     }
 }
