d217cec8825991e507a895f6fe753e233bb3aa87
ezopus <133430790+ezopus@users.noreply.github.com>
2024-10-27 18:53:32 +0200

    farmer class  populated

--- FarmersMarketApp.Data/Data/Models/Farmer.cs
+++ FarmersMarketApp.Data/Data/Models/Farmer.cs
@@ -1,7 +1,18 @@
-﻿namespace FarmersMarketApp.Infrastructure.Data.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FarmersMarketApp.Infrastructure.Data.Models
 {
     public class Farmer : ApplicationUser
     {
+        [Comment("Flag to show if farmer has any products for sale.")]
+        public bool HasProducts { get; set; }
 
+        [Comment("Flag to show if farmer is currently accepting deliveries.")]
+        public bool AcceptsDeliveries { get; set; }
+
+        [Comment("Unique identifier of farm which current farmer works on and produces products.")]
+        public Guid FarmId { get; set; }
+
+        public Farm Farm { get; set; } = null!;
     }
 }
