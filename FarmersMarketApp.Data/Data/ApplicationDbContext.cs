b098b6605920e8ae2f993be439b653a9532ecef5
ezopus <133430790+ezopus@users.noreply.github.com>
2024-10-27 20:33:53 +0200

    dbsets added in context

--- FarmersMarketApp.Data/Data/ApplicationDbContext.cs
+++ FarmersMarketApp.Data/Data/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using FarmersMarketApp.Infrastructure.Data.Configuration;
+using FarmersMarketApp.Infrastructure.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace FarmersMarketApp.Infrastructure.Data
@@ -8,6 +10,32 @@ namespace FarmersMarketApp.Infrastructure.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+
         }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfiguration(new CategoryConfiguration());
+            builder.ApplyConfiguration(new CategoryFarmerConfiguration());
+            builder.ApplyConfiguration(new ProductOrderConfiguration());
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Farmer> Farmers { get; set; }
+
+        public DbSet<Farm> Farms { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<CategoryFarmer> CategoriesFarmers { get; set; }
+
+        public DbSet<Payment> Payments { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<ProductOrder> ProductsOrders { get; set; }
     }
 }
