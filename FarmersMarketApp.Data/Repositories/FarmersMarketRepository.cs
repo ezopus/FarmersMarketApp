3024b234c536f00362c2a076b88c83108dc48508
ezopus <133430790+ezopus@users.noreply.github.com>
2024-10-28 22:11:45 +0200

    repository pattern added

---
+++ FarmersMarketApp.Data/Repositories/FarmersMarketRepository.cs
@@ -0,0 +1,58 @@
+﻿using FarmersMarketApp.Infrastructure.Data;
+using FarmersMarketApp.Infrastructure.Repositories.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace FarmersMarketApp.Infrastructure.Repositories
+{
+    public class FarmersMarketRepository : IRepository
+    {
+        private readonly DbContext context;
+
+        public FarmersMarketRepository(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        private DbSet<T> DbSet<T>() where T : class
+        {
+            return context.Set<T>();
+        }
+
+        public IQueryable<T> All<T>() where T : class
+        {
+            return DbSet<T>();
+        }
+
+        public IQueryable<T> AllReadOnly<T>() where T : class
+        {
+            return DbSet<T>()
+                .AsNoTracking();
+        }
+
+        public async Task AddAsync<T>(T entity) where T : class
+        {
+            await DbSet<T>().AddAsync(entity);
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await context.SaveChangesAsync();
+        }
+
+        public async Task<T?> GetByIdAsync<T>(object id) where T : class
+        {
+            return await DbSet<T>().FindAsync(id);
+        }
+
+        public async Task DeleteAsync<T>(object id) where T : class
+        {
+            T? entity = await GetByIdAsync<T>(id);
+
+            if (entity != null)
+            {
+                DbSet<T>().Remove(entity);
+            }
+        }
+
+    }
+}
