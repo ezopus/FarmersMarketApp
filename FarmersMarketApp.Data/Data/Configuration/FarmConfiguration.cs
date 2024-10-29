7aa95efac7e54065b6c71928aacea9eb020c88da
ezopus <133430790+ezopus@users.noreply.github.com>
2024-10-30 00:09:48 +0200

    farms seeded, migration added

--- FarmersMarketApp.Data/Data/Configuration/FarmConfiguration.cs
+++ FarmersMarketApp.Data/Data/Configuration/FarmConfiguration.cs
@@ -1,11 +1,14 @@
 ﻿using FarmersMarketApp.Infrastructure.Data.Models;
+using FarmersMarketApp.Infrastructure.Datasets.ImportDTOs;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Text.Json;
 
 namespace FarmersMarketApp.Infrastructure.Data.Configuration
 {
     public class FarmConfiguration : IEntityTypeConfiguration<Farm>
     {
+        private const string FarmDataSet = "../FarmersMarketApp.Data/Datasets/farms.json";
         public void Configure(EntityTypeBuilder<Farm> builder)
         {
             builder
@@ -14,6 +17,38 @@ namespace FarmersMarketApp.Infrastructure.Data.Configuration
                 .HasForeignKey<Farmer>()
                 .OnDelete(DeleteBehavior.NoAction);
 
+            var farms = LoadJsonData(FarmDataSet);
+
+            builder.HasData(farms);
+        }
+
+        private Farm[] LoadJsonData(string filePath)
+        {
+            var importJson = File.ReadAllText(filePath);
+            var importFarm = JsonSerializer.Deserialize<ImportFarmDto[]>(importJson);
+
+            ICollection<Farm> farms = new HashSet<Farm>();
+
+            foreach (var farm in importFarm)
+            {
+                var newFarm = new Farm
+                {
+                    Id = Guid.NewGuid(),
+                    Name = farm.Name,
+                    Address = farm.Address,
+                    City = farm.City,
+                    Email = farm.Email,
+                    PhoneNumber = farm.PhoneNumber,
+                    OpenHours = TimeOnly.Parse(farm.OpenHours),
+                    CloseHours = TimeOnly.Parse(farm.CloseHours),
+                    IsOpen = farm.IsOpen,
+                    FarmerId = Guid.Parse(farm.FarmerId),
+                };
+
+                farms.Add(newFarm);
+            }
+
+            return farms.ToArray();
         }
     }
 }
---
+++ FarmersMarketApp.Data/Datasets/ImportDTOs/ImportFarmDto.cs
@@ -0,0 +1,23 @@
+﻿namespace FarmersMarketApp.Infrastructure.Datasets.ImportDTOs
+{
+    public class ImportFarmDto
+    {
+        public required string Name { get; set; }
+
+        public required string Address { get; set; }
+
+        public required string City { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+        public string? OpenHours { get; set; }
+
+        public string? CloseHours { get; set; }
+
+        public bool IsOpen { get; set; }
+
+        public required string FarmerId { get; set; }
+    }
+}
