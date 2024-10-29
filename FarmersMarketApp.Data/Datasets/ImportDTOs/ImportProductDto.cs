d36c863edf650555f65795f8a41cca39e8506c0d
ezopus <133430790+ezopus@users.noreply.github.com>
2024-10-30 00:10:07 +0200

    import product dto added

---
+++ FarmersMarketApp.Data/Datasets/ImportDTOs/ImportProductDto.cs
@@ -0,0 +1,27 @@
+﻿namespace FarmersMarketApp.Infrastructure.Datasets.ImportDTOs
+{
+    public class ImportProductDto
+    {
+        public required string Id { get; set; }
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public required string UnitType { get; set; }
+        public required double Size { get; set; }
+        public required double Quantity { get; set; }
+        public required double NetWeight { get; set; }
+        public required double ShippingWeight { get; set; }
+        public string? Season { get; set; }
+        public required string ProductionDate { get; set; }
+        public required string ExpirationDate { get; set; }
+        public required int CategoryId { get; set; }
+        public required decimal Price { get; set; }
+        public bool HasDiscount { get; set; } = false;
+        public decimal? DiscountPercentage { get; set; }
+        public required string FarmerId { get; set; }
+        public required string FarmId { get; set; }
+        public string? Barcode { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? Origin { get; set; }
+
+    }
+}
