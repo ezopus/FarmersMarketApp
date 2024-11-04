dcdafbd76e5269de2f1a0acbf8a802f9f230a7f9
ezopus <133430790+ezopus@users.noreply.github.com>
2024-11-04 14:40:32 +0200

    added basic product info view model

--- FarmersMarketApp.Web.ViewModels/Product/ProductInfoViewModel.cs
+++ FarmersMarketApp.Web.ViewModels/Product/ProductInfoViewModel.cs
@@ -4,12 +4,12 @@ namespace FarmersMarketApp.Web.ViewModels.Product
 {
     public class ProductInfoViewModel
     {
-        public string Id { get; set; }
-        public string ProductName { get; set; }
+        public string Id { get; set; } = null!;
+        public string ProductName { get; set; } = null!;
 
-        public string Description { get; set; }
+        public string? Description { get; set; }
 
-        public string UnitType { get; set; }
+        public string UnitType { get; set; } = null!;
 
         public decimal Price { get; set; }
 
@@ -19,21 +19,21 @@ namespace FarmersMarketApp.Web.ViewModels.Product
 
         public int CategoryId { get; set; }
 
-        public List<Category> Categories { get; set; }
+        public List<Category> Categories { get; set; } = new List<Category>();
 
         public bool HasDiscount { get; set; }
         public decimal DiscountPercentage { get; set; }
 
-        public string ImageUrl { get; set; }
+        public string? ImageUrl { get; set; }
 
-        public string Origin { get; set; }
+        public string? Origin { get; set; }
 
-        public string FarmerId { get; set; }
+        public string FarmerId { get; set; } = null!;
 
-        public Farmer Farmer { get; set; }
+        public Farmer Farmer { get; set; } = null!;
 
-        public string FarmId { get; set; }
-        public Farm Farm { get; set; }
+        public string FarmId { get; set; } = null!;
+        public Farm Farm { get; set; } = null!;
 
 
     }
