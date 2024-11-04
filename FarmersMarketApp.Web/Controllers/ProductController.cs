76f43686eb122a5ba73983e031c85c48bf5f8f83
ezopus <133430790+ezopus@users.noreply.github.com>
2024-11-04 14:40:02 +0200

    added action All to Product controller with basic functionality

--- FarmersMarketApp.Web/Controllers/ProductController.cs
+++ FarmersMarketApp.Web/Controllers/ProductController.cs
@@ -1,4 +1,6 @@
 ﻿using FarmersMarketApp.Infrastructure.Data;
+using FarmersMarketApp.Infrastructure.Data.Models;
+using FarmersMarketApp.Web.ViewModels.Product;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -19,9 +21,31 @@ namespace FarmersMarketApp.Web.Controllers
                 .Include(p => p.Farm)
                 .Include(p => p.Farmer)
                 .Include(p => p.Farmer.User)
+                .Select(p => new ProductInfoViewModel()
+                {
+                    ProductName = p.Name,
+                    Description = p.Description,
+                    FarmId = p.FarmId.ToString(),
+                    Farm = p.Farm,
+                    FarmerId = p.FarmerId.ToString(),
+                    Farmer = p.Farmer,
+                    CategoryId = p.CategoryId,
+                    Price = p.Price,
+                    DiscountPercentage = p.DiscountPercentage ?? 0,
+                    UnitType = p.UnitType.ToString(),
+                    Size = p.Size,
+                    Quantity = p.Quantity,
+                    Origin = p.Origin ?? "",
+                    ImageUrl = p.ImageUrl ?? "",
+                })
                 .ToList();
 
             return View(products);
+        }
+
+        private async Task<List<Category>> GetCategories()
+        {
+            return await context.Categories.ToListAsync();
         }
     }
 }
