77703c85ac4c4e3935fb6b59c68af625a60b8cbd
ezopus <133430790+ezopus@users.noreply.github.com>
2024-11-04 14:39:43 +0200

    implemented basic product services and interface

--- FarmersMarketApp.Services/Contracts/IProductService.cs
+++ FarmersMarketApp.Services/Contracts/IProductService.cs
@@ -1,8 +1,19 @@
-﻿namespace FarmersMarketApp.Services.Contracts
+﻿using FarmersMarketApp.Infrastructure.Data.Models;
+
+namespace FarmersMarketApp.Services.Contracts
 {
     public interface IProductService
     {
-        Task GetProductsAsync<T>();
+        Task<IEnumerable<Product>> GetProductsAsync();
 
+        Task<Product?> GetProductById(Guid id);
+
+        Task<Product?> GetProductByName(string name);
+
+        Task<IEnumerable<Product>> GetProductsByFarmerId(Guid farmerId);
+
+        Task<IEnumerable<Product>> GetProductsByFarmId(Guid farmId);
+
+        Task<IEnumerable<Product>> GetProductsByCategoryId(int categoryId);
     }
 }
--- FarmersMarketApp.Services/ProductService.cs
+++ FarmersMarketApp.Services/ProductService.cs
@@ -1,12 +1,55 @@
-﻿using FarmersMarketApp.Services.Contracts;
+﻿using FarmersMarketApp.Infrastructure.Data.Models;
+using FarmersMarketApp.Infrastructure.Repositories;
+using FarmersMarketApp.Services.Contracts;
+using Microsoft.EntityFrameworkCore;
 
 namespace FarmersMarketApp.Services
 {
     public class ProductService : IProductService
     {
-        public Task GetProductsAsync<T>()
-        {
+        private readonly FarmersMarketRepository repository;
 
+        public ProductService(FarmersMarketRepository repository)
+        {
+            this.repository = repository;
+        }
+        public async Task<IEnumerable<Product>> GetProductsAsync()
+        {
+            return await repository.AllReadOnly<Product>().ToListAsync();
+        }
+
+        public async Task<Product?> GetProductById(Guid id)
+        {
+            return await repository.All<Product>().FirstOrDefaultAsync(pr => pr.Id == id);
+        }
+
+        public async Task<Product?> GetProductByName(string name)
+        {
+            return await repository.All<Product>().FirstOrDefaultAsync(pr => pr.Name == name);
+        }
+
+        public async Task<IEnumerable<Product>> GetProductsByFarmerId(Guid farmerId)
+        {
+            return await repository
+                .All<Product>()
+                .Where(pr => pr.Farmer.Id == farmerId)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductsByFarmId(Guid farmId)
+        {
+            return await repository
+                .All<Product>()
+                .Where(pr => pr.Farm.Id == farmId)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductsByCategoryId(int categoryId)
+        {
+            return await repository
+                .All<Product>()
+                .Where(pr => pr.CategoryId == categoryId)
+                .ToListAsync(); ;
         }
     }
 }
