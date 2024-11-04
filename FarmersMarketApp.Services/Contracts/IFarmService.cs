cc82b9ef35a63dbf8001e4e889bf8b080d389445
ezopus <133430790+ezopus@users.noreply.github.com>
2024-11-04 13:52:37 +0200

    farmer services interface added

---
+++ FarmersMarketApp.Services/Contracts/IFarmService.cs
@@ -0,0 +1,13 @@
+﻿using FarmersMarketApp.Infrastructure.Data.Models;
+
+namespace FarmersMarketApp.Services.Contracts
+{
+    public interface IFarmService
+    {
+        Task<IEnumerable<Farm>> GetFarmsAsync();
+
+        Task<Farm> GetFarmByIdAsync(Guid id);
+
+        Task<IEnumerable<Farm>> GetFarmsByFarmerIdAsync(Guid farmerId);
+    }
+}
