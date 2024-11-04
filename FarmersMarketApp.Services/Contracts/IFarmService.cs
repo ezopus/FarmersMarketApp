f2531d80f3a140b68814a24c41938ffc2c4c43a3
ezopus <133430790+ezopus@users.noreply.github.com>
2024-11-04 18:45:41 +0200

    added get all farms async service

--- FarmersMarketApp.Services/Contracts/IFarmService.cs
+++ FarmersMarketApp.Services/Contracts/IFarmService.cs
@@ -1,10 +1,11 @@
 ﻿using FarmersMarketApp.Infrastructure.Data.Models;
+using FarmersMarketApp.Web.ViewModels.FarmViewModels;
 
 namespace FarmersMarketApp.Services.Contracts
 {
     public interface IFarmService
     {
-        Task<IEnumerable<Farm>> GetFarmsAsync();
+        Task<IEnumerable<FarmInfoViewModel>> GetFarmsAsync();
 
         Task<Farm?> GetFarmByIdAsync(Guid id);
 
--- FarmersMarketApp.Services/FarmService.cs
+++ FarmersMarketApp.Services/FarmService.cs
@@ -1,6 +1,7 @@
 ﻿using FarmersMarketApp.Infrastructure.Data.Models;
 using FarmersMarketApp.Infrastructure.Repositories.Contracts;
 using FarmersMarketApp.Services.Contracts;
+using FarmersMarketApp.Web.ViewModels.FarmViewModels;
 using Microsoft.EntityFrameworkCore;
 
 namespace FarmersMarketApp.Services
@@ -12,9 +13,26 @@ namespace FarmersMarketApp.Services
         {
             this.repository = repository;
         }
-        public async Task<IEnumerable<Farm>> GetFarmsAsync()
+        public async Task<IEnumerable<FarmInfoViewModel>> GetFarmsAsync()
         {
-            return await repository.AllAsync<Farm>().ToListAsync();
+            return await repository
+                .AllAsync<Farm>()
+                .Select(f => new FarmInfoViewModel()
+                {
+                    Id = f.Id.ToString(),
+                    Name = f.Name,
+                    Address = f.Address,
+                    PhoneNumber = f.PhoneNumber,
+                    City = f.City,
+                    CloseHours = f.CloseHours.ToString(),
+                    OpenHours = f.OpenHours.ToString(),
+                    Email = f.Email,
+                    ImageUrl = f.ImageUrl,
+                    IsOpen = f.IsOpen,
+                    FarmersFarms = f.FarmersFarms,
+                    Products = f.Products,
+                })
+                .ToListAsync();
         }
 
         public async Task<Farm?> GetFarmByIdAsync(Guid id)
