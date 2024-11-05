4fef14134498cfd279e7a8afa10064e4c9adabce
ezopus <133430790+ezopus@users.noreply.github.com>
2024-11-05 17:01:20 +0200

    added services to service collection extension

--- FarmersMarketApp.Web/Extensions/ServiceCollectionExtensions.cs
+++ FarmersMarketApp.Web/Extensions/ServiceCollectionExtensions.cs
@@ -1,59 +1,61 @@
 ﻿namespace FarmersMarketApp.Web.Extensions
 {
-    using FarmersMarketApp.Infrastructure.Data;
-    using FarmersMarketApp.Infrastructure.Data.Models;
-    using FarmersMarketApp.Infrastructure.Repositories;
-    using FarmersMarketApp.Infrastructure.Repositories.Contracts;
-    using FarmersMarketApp.Services;
-    using FarmersMarketApp.Services.Contracts;
-    using Microsoft.AspNetCore.Identity;
-    using Microsoft.EntityFrameworkCore;
-    using Microsoft.Extensions.DependencyInjection;
-    public static class ServiceCollectionExtensions
-    {
-        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
-        {
-            services.AddScoped<IProductService, ProductService>();
-            services.AddScoped<IFarmService, FarmService>();
+	using FarmersMarketApp.Infrastructure.Data;
+	using FarmersMarketApp.Infrastructure.Data.Models;
+	using FarmersMarketApp.Infrastructure.Repositories;
+	using FarmersMarketApp.Infrastructure.Repositories.Contracts;
+	using FarmersMarketApp.Services;
+	using FarmersMarketApp.Services.Contracts;
+	using Microsoft.AspNetCore.Identity;
+	using Microsoft.EntityFrameworkCore;
+	using Microsoft.Extensions.DependencyInjection;
+	public static class ServiceCollectionExtensions
+	{
+		public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+		{
+			services.AddScoped<IProductService, ProductService>();
+			services.AddScoped<IFarmService, FarmService>();
+			services.AddScoped<IUserService, UserService>();
+			services.AddScoped<IFarmerService, FarmerService>();
 
-            return services;
-        }
+			return services;
+		}
 
-        public static IServiceCollection AddApplicationDbContext(this IServiceCollection services,
-            IConfiguration configuration)
-        {
-            var connectionString = configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+		public static IServiceCollection AddApplicationDbContext(this IServiceCollection services,
+			IConfiguration configuration)
+		{
+			var connectionString = configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
-            services
-                .AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+			services
+				.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 
-            services.AddScoped<IRepository, FarmersMarketRepository>();
+			services.AddScoped<IRepository, FarmersMarketRepository>();
 
-            //services.AddDatabaseDeveloperPageExceptionFilter();
+			//services.AddDatabaseDeveloperPageExceptionFilter();
 
-            return services;
-        }
+			return services;
+		}
 
-        public static IServiceCollection AddApplicationIdentity(this IServiceCollection services,
-            IConfiguration configuration)
-        {
-            services
-                .AddIdentity<ApplicationUser, IdentityRole<Guid>>(options =>
-                {
-                    options.SignIn.RequireConfirmedAccount = false;
-                    options.SignIn.RequireConfirmedEmail = false;
+		public static IServiceCollection AddApplicationIdentity(this IServiceCollection services,
+			IConfiguration configuration)
+		{
+			services
+				.AddIdentity<ApplicationUser, IdentityRole<Guid>>(options =>
+				{
+					options.SignIn.RequireConfirmedAccount = false;
+					options.SignIn.RequireConfirmedEmail = false;
 
-                    options.Password.RequireDigit = false;
-                    options.Password.RequireLowercase = false;
-                    options.Password.RequireUppercase = false;
-                    options.Password.RequireNonAlphanumeric = false;
-                })
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+					options.Password.RequireDigit = false;
+					options.Password.RequireLowercase = false;
+					options.Password.RequireUppercase = false;
+					options.Password.RequireNonAlphanumeric = false;
+				})
+				.AddEntityFrameworkStores<ApplicationDbContext>();
 
 
 
-            return services;
-        }
+			return services;
+		}
 
-    }
+	}
 }
