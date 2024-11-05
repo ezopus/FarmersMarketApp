74af149f112b859b5f4e0ea5e52ae6e20ffab968
ezopus <133430790+ezopus@users.noreply.github.com>
2024-11-05 16:59:34 +0200

    become farmer view added with farmer view model

---
+++ FarmersMarketApp.Web.ViewModels/FarmerViewModels/FarmerBecomeViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace FarmersMarketApp.Web.ViewModels.FarmerViewModels
+{
+	public class FarmerBecomeViewModel
+	{
+		public Guid UserId { get; set; }
+
+		public bool HasProducts { get; set; } = false;
+
+		public bool? AcceptsDeliveries { get; set; } = null;
+
+		public string? CompanyName { get; set; }
+
+		public string? CompanyRegistrationNumber { get; set; }
+
+		public string? CompanyAddress { get; set; }
+
+		//TODO: Try to implement farmer category from the registration
+		//public List<int> Categories { get; set; }
+	}
+}
