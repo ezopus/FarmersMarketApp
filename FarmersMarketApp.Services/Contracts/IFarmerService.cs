8c15eaa811ac850e8a7e2fa1251e9943b00b4f31
ezopus <133430790+ezopus@users.noreply.github.com>
2024-11-05 16:59:46 +0200

    farmer services added

---
+++ FarmersMarketApp.Services/Contracts/IFarmerService.cs
@@ -0,0 +1,11 @@
+﻿using FarmersMarketApp.Infrastructure.Data.Models;
+using FarmersMarketApp.Web.ViewModels.FarmerViewModels;
+
+namespace FarmersMarketApp.Services.Contracts
+{
+	public interface IFarmerService
+	{
+		Task<Guid> BecomeFarmerAsync(ApplicationUser user, FarmerBecomeViewModel model);
+
+	}
+}
---
+++ FarmersMarketApp.Services/FarmerService.cs
@@ -0,0 +1,38 @@
+﻿using FarmersMarketApp.Infrastructure.Data.Models;
+using FarmersMarketApp.Infrastructure.Repositories.Contracts;
+using FarmersMarketApp.Services.Contracts;
+using FarmersMarketApp.Web.ViewModels.FarmerViewModels;
+
+namespace FarmersMarketApp.Services
+{
+	public class FarmerService : IFarmerService
+	{
+		private readonly IRepository repository;
+		public FarmerService(IRepository repository)
+		{
+			this.repository = repository;
+		}
+		public async Task<Guid> BecomeFarmerAsync(ApplicationUser user, FarmerBecomeViewModel model)
+		{
+			var newFarmer = new Farmer()
+			{
+				Id = Guid.NewGuid(),
+				UserId = user.Id,
+				CompanyName = model.CompanyName,
+				CompanyAddress = model.CompanyAddress,
+				CompanyRegistrationNumber = model.CompanyRegistrationNumber,
+				AcceptsDeliveries = model.AcceptsDeliveries.Value,
+				HasProducts = model.HasProducts,
+			};
+
+			user.IsFarmer = true;
+
+			await repository.AddAsync<Farmer>(newFarmer);
+			await repository.SaveChangesAsync();
+
+			return newFarmer.Id;
+		}
+
+
+	}
+}
