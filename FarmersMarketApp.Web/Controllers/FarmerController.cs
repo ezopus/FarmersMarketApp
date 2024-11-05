589ea748914438f02451f95a2ee425c530009469
ezopus <133430790+ezopus@users.noreply.github.com>
2024-11-05 17:00:14 +0200

    added farmer services to farmer controller

--- FarmersMarketApp.Web/Controllers/FarmerController.cs
+++ FarmersMarketApp.Web/Controllers/FarmerController.cs
@@ -1,12 +1,66 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FarmersMarketApp.Infrastructure.Repositories.Contracts;
+using FarmersMarketApp.Services.Contracts;
+using FarmersMarketApp.Web.Extensions;
+using FarmersMarketApp.Web.ViewModels.FarmerViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FarmersMarketApp.Web.Controllers
 {
-    public class FarmerController : BaseController
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
-    }
+	public class FarmerController : BaseController
+	{
+		private readonly IRepository repository;
+		private readonly IFarmService farmService;
+		private readonly IFarmerService farmerService;
+		private readonly IUserService userService;
+
+		public FarmerController(
+			IRepository repository,
+			IUserService userService,
+			IFarmerService farmerService,
+			IFarmService farmService)
+		{
+			this.repository = repository;
+			this.farmerService = farmerService;
+			this.userService = userService;
+		}
+		public async Task<IActionResult> Index()
+		{
+			var currentUserId = User.GetId();
+
+
+			return View();
+		}
+
+		[HttpGet]
+		public IActionResult Become()
+		{
+			if (User.Identity?.IsAuthenticated == null)
+			{
+				return RedirectToPage("/Account/Login");
+			}
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Become(FarmerBecomeViewModel model)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View(model);
+			}
+
+			var currentUserId = User.GetId();
+			var currentUser = await userService.GetCurrentUser(Guid.Parse(currentUserId));
+
+			if (currentUser == null || currentUser.IsFarmer)
+			{
+				return RedirectToAction(nameof(Index), "Home");
+			}
+
+			await farmerService.BecomeFarmerAsync(currentUser, model);
+
+			return RedirectToAction(nameof(Index));
+		}
+
+	}
 }
