c84e7d998c7c94725e394373e2748652db8258df
ezopus <133430790+ezopus@users.noreply.github.com>
2024-11-07 16:41:41 +0200

    js script added to switch chosen farms while adding product

---
+++ FarmersMarketApp.Web/wwwroot/js/farmSwitcher.js
@@ -0,0 +1,19 @@
+﻿import { API_URL } from "/js/constants.js";
+
+var dropDown = document.getElementById('farmDropDown');
+
+dropDown.addEventListener('change', async () => {
+
+    var farmerId = document.getElementById("farmerId").value;
+    var response = await fetch(API_URL + farmerId);
+    var farms = await response.json();
+    var farmId = dropDown.value;
+
+    // Update the farm details
+    document.getElementById('farmName').textContent = farms[farmId].name;
+    document.getElementById('farmAddress').textContent = farms[farmId].address;
+    document.getElementById('farmCity').textContent = farms[farmId].city;
+    var img = document.getElementById('farmImage');
+    img.src = farms[farmId].imageUrl;
+    img.alt = farms[farmId].name;
+});
