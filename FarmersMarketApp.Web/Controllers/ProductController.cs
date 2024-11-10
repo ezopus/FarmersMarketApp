c171d42a9b039c0c88dbfedf85ef44435bb082bd
ezopus <133430790+ezopus@users.noreply.github.com>
2024-11-10 16:00:36 +0200

    added validations to product controller

--- FarmersMarketApp.Web/Controllers/ProductController.cs
+++ FarmersMarketApp.Web/Controllers/ProductController.cs
@@ -31,14 +31,16 @@ namespace FarmersMarketApp.Web.Controllers
 		[HttpGet]
 		public async Task<IActionResult> All(string? farmerId, string? farmerFullName, string? farmId, string? farmName)
 		{
+			//check if user wants only products by one farmer
 			if (!string.IsNullOrEmpty(farmerId))
 			{
 				ViewData["farmerId"] = farmerId;
 				ViewData["farmerFullName"] = farmerFullName;
-				var model = await productService.GetProductsByFarmerIdAsync(Guid.Parse(farmerId));
+				var model = await productService.GetProductsByFarmerIdAsync(farmerId);
 				return View(model);
 			}
 
+			//check if user wants only products by one farm
 			if (!string.IsNullOrEmpty(farmId))
 			{
 				ViewData["farmName"] = farmName;
@@ -46,6 +48,7 @@ namespace FarmersMarketApp.Web.Controllers
 				return View(model);
 			}
 
+			//gets all products which are not deleted
 			var allModels = await productService.GetProductsAsync();
 
 			return View(allModels);
@@ -55,7 +58,7 @@ namespace FarmersMarketApp.Web.Controllers
 		public async Task<IActionResult> Add()
 		{
 			//get current userId
-			var currentUserId = Guid.Parse(User.GetId());
+			var currentUserId = User.GetId();
 			var farmerId = await farmerService.GetFarmerIdByUserIdAsync(currentUserId);
 
 			//check if user is a farmer, if not, redirect him to become a farmer
@@ -65,10 +68,10 @@ namespace FarmersMarketApp.Web.Controllers
 			}
 
 			//get all farms where farmer is registered
-			var farmerFarms = await farmService.GetFarmNameAndIdForNewProductAsync(farmerId.Value);
+			var farmerFarms = await farmService.GetFarmNameAndIdForNewProductAsync(farmerId);
 
 			//check if farmer has farms, if not - redirect him to add a farm
-			if (!farmerFarms.Any())
+			if (farmerFarms == null || !farmerFarms.Any())
 			{
 				return RedirectToAction(nameof(Add), "Farm");
 			}
@@ -79,18 +82,40 @@ namespace FarmersMarketApp.Web.Controllers
 			model.UnitTypes = new List<UnitType>((UnitType[])Enum.GetValues(typeof(UnitType)));
 			model.Seasons = new List<Season>((Season[])Enum.GetValues(typeof(Season)));
 			model.Farms = farmerFarms;
-			model.FarmerId = farmerId.Value;
+			model.FarmerId = farmerId;
 
 			return View(model);
 		}
 
-
+		//TODO: check validations
 		[HttpPost]
+		//TODO: add anti-forgery token here
 		public async Task<IActionResult> Add(AddProductViewModel model)
 		{
-			var currentUserId = Guid.Parse(User.GetId());
+			var currentUserId = User.GetId();
+
+			//if user is not logged, redirect him to login
+			if (string.IsNullOrEmpty(currentUserId))
+			{
+				return RedirectToPage("/Account/Login", new { area = "Identity" });
+			}
+
 			var currentFarmerId = await farmerService.GetFarmerIdByUserIdAsync(currentUserId);
 
+			//check if user is a farmer, if not redirect him to become one
+			if (string.IsNullOrEmpty(currentFarmerId))
+			{
+				return RedirectToAction("Become", "Farmer");
+			}
+
+			//check if current farmer is working on farm to add product to, if no farms are found redirect him to add farm
+			var farmerFarms = await farmService.GetOnlyFarmIdsByFarmerId(currentFarmerId);
+			if (farmerFarms == null || !farmerFarms.Any())
+			{
+				return RedirectToAction("Add", "Farm");
+			}
+
+			//check model state
 			if (!ModelState.IsValid)
 			{
 				model.Categories = await categoryService.GetCategoriesAsync();
@@ -100,85 +125,120 @@ namespace FarmersMarketApp.Web.Controllers
 			}
 
 
-			//check if current user is farmer
-			if (currentFarmerId == null || model.FarmerId != currentFarmerId)
+			//check if current user is farmer and if farmer is allowed to add to this farm, if farmer is not allowed redirect him to his own farms
+			if (model.FarmerId != currentFarmerId)
 			{
-				return RedirectToAction(nameof(All));
+				return RedirectToAction("MyFarms", "Farmer");
 			}
 
-			//check if current farmer is working on farm to add product to
-			var farmerFarms = await farmService.GetFarmIdsByFarmerId(currentFarmerId.Value);
-			if (!farmerFarms.Any())
-			{
-				return RedirectToAction(nameof(All));
-			}
-
+			//try adding a product
 			var result = await productService.CreateProductAsync(model);
 
-			if (result == null)
+			//check if successfully added product
+			if (string.IsNullOrEmpty(result))
 			{
 				return View(model);
 			}
 
-			return RedirectToAction("Details", "Product", new { id = result.Value });
+			//return product details page to look at newly added product
+			return RedirectToAction("Details", "Product", new { id = result });
 		}
 
-		//TODO: Implement categories
 
 		[AllowAnonymous]
 		[HttpGet]
-		public async Task<IActionResult> Details(Guid id)
+		public async Task<IActionResult> Details(string id)
 		{
+			//get product from db, if not found redirect to all products
 			var model = await productService.GetProductByIdAsync(id);
 			if (model == null)
 			{
 				return RedirectToAction("All");
 			}
 
-			model.Farm = await farmService.GetFarmByIdReadOnlyAsync(Guid.Parse(model.FarmId));
-			model.Farmer = await farmerService.GetFarmerIdByAsync(model.FarmerId);
+			//try to get farm of product
+			var modelFarm = await farmService.GetFarmByIdReadOnlyAsync(model.FarmId);
+			if (modelFarm != null)
+			{
+				model.Farm = modelFarm;
+			}
+
+			//try to get farmer of product
+			var modelFarmer = await farmerService.GetFarmerByIdAsync(model.FarmerId);
+			if (modelFarmer != null)
+			{
+				model.Farmer = modelFarmer;
+			}
 
 			return View(model);
 		}
 
 		[HttpGet]
-		public async Task<IActionResult> Edit(Guid productId)
+		public async Task<IActionResult> Edit(string productId)
 		{
-			var model = await productService.GetProductToEditByIdAsync(productId);
-
-			if (model == null)
-			{
-				return RedirectToAction(nameof(All));
-			}
-
-			var currentUserId = Guid.Parse(User.GetId());
+			var currentUserId = User.GetId();
 			var currentFarmerId = await farmerService.GetFarmerIdByUserIdAsync(currentUserId);
 
-			if (currentFarmerId == null)
+			//check if user is a farmer, if not redirect him to become one
+			if (string.IsNullOrEmpty(currentFarmerId))
 			{
-				return RedirectToAction(nameof(All));
+				return RedirectToAction("Become", "Farmer");
 			}
-			var currentFarmerFarms = await farmService.GetFarmIdsByFarmerId(currentFarmerId.Value);
 
-			if (!currentFarmerFarms.Any(f => f.Equals(model.FarmId.ToString(), StringComparison.CurrentCultureIgnoreCase)))
+			//get model from db, if not found redirect to all products
+			var model = await productService.GetProductToEditByIdAsync(productId);
+			if (model == null)
 			{
-				return RedirectToAction(nameof(All));
+				return RedirectToAction("All");
+			}
+
+			//get all farms of farmer where he is employed
+			var currentFarmerFarms = await farmService.GetOnlyFarmIdsByFarmerId(currentFarmerId);
+
+			//check if any farm where farmer works is producing this product
+			if (currentFarmerFarms == null || !currentFarmerFarms.Any(f => f.Equals(model.FarmId, StringComparison.CurrentCultureIgnoreCase)))
+			{
+				return RedirectToAction("MyProducts", "Farmer");
 			}
 
 			model.Categories = await categoryService.GetCategoriesAsync();
 			model.UnitTypes = new List<UnitType>((UnitType[])Enum.GetValues(typeof(UnitType)));
 			model.Seasons = new List<Season>((Season[])Enum.GetValues(typeof(Season)));
-			model.Farms = await farmService.GetFarmNameAndIdForNewProductAsync(currentFarmerId.Value);
+			model.Farms = await farmService.GetFarmNameAndIdForNewProductAsync(currentFarmerId);
 
 			return View(model);
 		}
 
 		[HttpPost]
+		//todo: add anti-forgery token here
 		public async Task<IActionResult> Edit(AddProductViewModel model)
 		{
-			var currentUserId = Guid.Parse(User.GetId());
+			var currentUserId = User.GetId();
+
+			//if user is not logged, redirect him to login
+			if (string.IsNullOrEmpty(currentUserId))
+			{
+				return RedirectToPage("/Account/Login", new { area = "Identity" });
+			}
+
 			var currentFarmerId = await farmerService.GetFarmerIdByUserIdAsync(currentUserId);
 
+			//check if user is a farmer, if not redirect him to become one
+			if (string.IsNullOrEmpty(currentFarmerId))
+			{
+				return RedirectToAction("Become", "Farmer");
+			}
+
+			//get all farms of farmer where he is employed
+			var currentFarmerFarms = await farmService.GetOnlyFarmIdsByFarmerId(currentFarmerId);
+
+			//check if any farm where farmer works is producing this product
+			if (currentFarmerFarms == null || !currentFarmerFarms.Any(f => f.Equals(model.FarmId, StringComparison.CurrentCultureIgnoreCase)))
+			{
+				return RedirectToAction("MyProducts", "Farmer");
+			}
+
+			//check model state on post
 			if (!ModelState.IsValid)
 			{
 				model.Categories = await categoryService.GetCategoriesAsync();
@@ -187,27 +247,16 @@ namespace FarmersMarketApp.Web.Controllers
 				return View(model);
 			}
 
-
-			//check if current user is farmer
-			if (currentFarmerId == null || model.FarmerId != currentFarmerId)
-			{
-				return RedirectToAction(nameof(All));
-			}
-
-			//check if current farmer is working on farm to add product to
-			var farmerFarms = await farmService.GetFarmIdsByFarmerId(currentFarmerId.Value);
-			if (!farmerFarms.Any())
-			{
-				return RedirectToAction(nameof(All));
-			}
-
+			//try to edit product
 			var result = await productService.UpdateEditedProductAsync(model);
 
+			//if edit fails return view
 			if (result == null)
 			{
 				return View(model);
 			}
 
+			//return details of edited product to verify edit is successful
 			return RedirectToAction("Details", "Product", new { id = model.Id });
 		}
 
