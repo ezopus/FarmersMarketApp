bb6ae07b229b568c4ebecb27f1cac8bfe062fbbe
ezopus <133430790+ezopus@users.noreply.github.com>
2024-11-10 15:57:59 +0200

    edited must be farmer attribute to accept string not guid

--- FarmersMarketApp.Web/Attributes/MustBeFarmer.cs
+++ FarmersMarketApp.Web/Attributes/MustBeFarmer.cs
@@ -5,28 +5,24 @@ using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace FarmersMarketApp.Web.Attributes
 {
-    public class MustBeFarmer : ActionFilterAttribute
-    {
-        public override void OnActionExecuting(ActionExecutingContext context)
-        {
-            base.OnActionExecuting(context);
+	public class MustBeFarmer : ActionFilterAttribute
+	{
+		public override void OnActionExecuting(ActionExecutingContext context)
+		{
+			base.OnActionExecuting(context);
 
-            IUserService? userService = context.HttpContext.RequestServices.GetService<IUserService>();
+			IUserService? userService = context.HttpContext.RequestServices.GetService<IUserService>();
 
-            if (userService != null)
-            {
-                context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
-            }
+			if (userService != null)
+			{
+				context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+			}
 
-            if (userService != null
-                && userService.IsUserFarmerAsync(
-                    Guid.Parse(
-                        context.HttpContext.User.GetId()
-                        )
-                    ).Result == false)
-            {
-                context.Result = new StatusCodeResult(StatusCodes.Status403Forbidden);
-            }
-        }
-    }
+			if (userService != null
+				&& userService.IsUserFarmerAsync(context.HttpContext.User.GetId()).Result == false)
+			{
+				context.Result = new StatusCodeResult(StatusCodes.Status403Forbidden);
+			}
+		}
+	}
 }
