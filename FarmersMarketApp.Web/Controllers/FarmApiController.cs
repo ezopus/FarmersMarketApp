701dfad68a02a4917d078d31060ec68a9eb8ead5
ezopus <133430790+ezopus@users.noreply.github.com>
2024-11-10 16:00:48 +0200

    added validations to farm api controller

--- FarmersMarketApp.Web/Controllers/FarmApiController.cs
+++ FarmersMarketApp.Web/Controllers/FarmApiController.cs
@@ -15,22 +15,35 @@ namespace FarmersMarketApp.Web.Controllers
 		}
 		// API action to get a specific farm's information
 		[HttpGet("info/{farmerId}")]
-		public async Task<IActionResult> GetFarmInfo(Guid farmerId)
+		public async Task<IActionResult> GetFarmInfo(string farmerId)
 		{
+			//get all farms where the farmer is participating
 			var farms = await farmService.GetFarmNameAndIdForNewProductAsync(farmerId);
-			Dictionary<Guid, object> farmsForApi = new();
 
-			foreach (var f in farms)
+			//the api will return this object
+			Dictionary<string, object> farmsForApi = new();
+
+			//if there are no farms, return a not found response
+			//todo: update js script to handle logic correctly when no response is found
+			if (farms == null)
 			{
-				farmsForApi.Add(f.Id, new { f.Name, f.Address, f.City, f.ImageUrl });
+				return NotFound();
 			}
 
+			//add farm info to json object
+			foreach (var f in farms)
+			{
+				farmsForApi.Add(f.Id.ToString(), new { f.Name, f.Address, f.City, f.ImageUrl });
+			}
+
+			//return object with three random farms and their info
 			return Ok(farmsForApi);
 		}
 
 		[HttpGet]
 		public IActionResult Index()
 		{
+			//test info if api loads correctly
 			return Ok(new { farmId = "test" });
 		}
 	}
