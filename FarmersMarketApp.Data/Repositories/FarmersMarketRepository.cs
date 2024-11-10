3cf46a580adc887677177d272fb8b87f8f4d83b3
ezopus <133430790+ezopus@users.noreply.github.com>
2024-11-10 23:58:27 +0200

    Cart controller removed for now, layout updated with proper buttons and first name visible when logged in

--- FarmersMarketApp.Data/Repositories/FarmersMarketRepository.cs
+++ FarmersMarketApp.Data/Repositories/FarmersMarketRepository.cs
@@ -4,54 +4,54 @@ using Microsoft.EntityFrameworkCore;
 
 namespace FarmersMarketApp.Infrastructure.Repositories
 {
-    public class FarmersMarketRepository : IRepository
-    {
-        private readonly DbContext context;
+	public class FarmersMarketRepository : IRepository
+	{
+		private readonly DbContext context;
 
-        public FarmersMarketRepository(ApplicationDbContext context)
-        {
-            this.context = context;
-        }
+		public FarmersMarketRepository(ApplicationDbContext context)
+		{
+			this.context = context;
+		}
 
-        private DbSet<T> DbSet<T>() where T : class
-        {
-            return context.Set<T>();
-        }
+		private DbSet<T> DbSet<T>() where T : class
+		{
+			return context.Set<T>();
+		}
 
-        public IQueryable<T> AllAsync<T>() where T : class
-        {
-            return DbSet<T>();
-        }
+		public IQueryable<T> AllAsync<T>() where T : class
+		{
+			return DbSet<T>();
+		}
 
-        public IQueryable<T> AllReadOnly<T>() where T : class
-        {
-            return DbSet<T>().AsNoTracking();
-        }
+		public IQueryable<T> AllReadOnly<T>() where T : class
+		{
+			return DbSet<T>().AsNoTracking();
+		}
 
-        public async Task AddAsync<T>(T entity) where T : class
-        {
-            await DbSet<T>().AddAsync(entity);
-        }
+		public async Task AddAsync<T>(T entity) where T : class
+		{
+			await DbSet<T>().AddAsync(entity);
+		}
 
-        public async Task<int> SaveChangesAsync()
-        {
-            return await context.SaveChangesAsync();
-        }
+		public async Task<int> SaveChangesAsync()
+		{
+			return await context.SaveChangesAsync();
+		}
 
-        public async Task<T?> GetByIdAsync<T>(object id) where T : class
-        {
-            return await DbSet<T>().FindAsync(id);
-        }
+		public async Task<T?> GetByIdAsync<T>(object id) where T : class
+		{
+			return await DbSet<T>().FindAsync(id);
+		}
 
-        public async Task DeleteAsync<T>(object id) where T : class
-        {
-            T? entity = await GetByIdAsync<T>(id);
+		public async Task DeleteAsync<T>(object id) where T : class
+		{
+			T? entity = await GetByIdAsync<T>(id);
 
-            if (entity != null)
-            {
-                DbSet<T>().Remove(entity);
-            }
-        }
+			if (entity != null)
+			{
+				DbSet<T>().Remove(entity);
+			}
+		}
 
-    }
+	}
 }
