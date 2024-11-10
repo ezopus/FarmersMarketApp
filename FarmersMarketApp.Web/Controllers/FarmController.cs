f695045e783a25b76841a25b06311e3f168f03f3
ezopus <133430790+ezopus@users.noreply.github.com>
2024-11-10 16:00:15 +0200

    added proper validations to farm controller

--- FarmersMarketApp.Web/Controllers/FarmController.cs
+++ FarmersMarketApp.Web/Controllers/FarmController.cs
@@ -23,20 +23,25 @@ namespace FarmersMarketApp.Web.Controllers
 
 		[AllowAnonymous]
 		[HttpGet]
-		public async Task<IActionResult> Index(Guid? farmerId)
+		public async Task<IActionResult> Index(string? farmerId)
 		{
 			var currentUserId = User.GetId();
-			var currentFarmerId = await farmerService.GetFarmerIdByUserIdAsync(Guid.Parse(currentUserId));
+			var currentFarmerId = "";
 
-			if (currentFarmerId != null)
+			if (!string.IsNullOrEmpty(currentUserId))
 			{
-				ViewData["farmerId"] = currentFarmerId.ToString();
+				currentFarmerId = await farmerService.GetFarmerIdByUserIdAsync(currentUserId);
+			}
+
+			if (!string.IsNullOrEmpty(currentFarmerId))
+			{
+				ViewData["farmerId"] = currentFarmerId;
 			}
 
 			//check if route has farmer id parameters and if yes show only farms by one farmer
 			//TODO: add switch case for filtration by category 
-			var farms = farmerId.HasValue
-				? await farmService.GetFarmsByFarmerIdAsync(farmerId.Value)
+			var farms = !string.IsNullOrEmpty(farmerId)
+				? await farmService.GetAllFarmsByFarmerIdAsync(farmerId)
 				: await farmService.GetFarmsAsync();
 
 			return View(farms);
@@ -45,16 +50,38 @@ namespace FarmersMarketApp.Web.Controllers
 		[HttpGet]
 		public async Task<IActionResult> Add()
 		{
+			var currentUserId = User.GetId();
+			var currentFarmerId = await farmerService.GetFarmerIdByUserIdAsync(currentUserId);
+
+			//ensure current user is a farmer, otherwise redirect him to become one
+			if (string.IsNullOrEmpty(currentFarmerId))
+			{
+				return RedirectToAction("Become", "Farmer");
+			}
+
 			return View();
 		}
+
 		[HttpPost]
+		//todo: add anti-forgery token to post action
 		public async Task<IActionResult> Add(AddFarmViewModel model)
 		{
+			var currentUserId = User.GetId();
+			var currentFarmerId = await farmerService.GetFarmerIdByUserIdAsync(currentUserId);
+
+			//ensure current user is a farmer, otherwise redirect him to become one
+			if (string.IsNullOrEmpty(currentFarmerId))
+			{
+				return RedirectToAction("Become", "Farmer");
+			}
+
+			//check model state of add farm model
 			if (!ModelState.IsValid)
 			{
 				return View(model);
 			}
 
+			//try to parse dates for open and close hours of farm and add errors to proper fields
 			if (model.OpenHours != null || model.CloseHours != null)
 			{
 				var isOpenHoursValid = TimeOnly.TryParseExact(model.OpenHours, TimeRequiredFormat,
@@ -72,43 +99,40 @@ namespace FarmersMarketApp.Web.Controllers
 					ModelState.AddModelError(nameof(model.CloseHours), ErrorFarmHours);
 					return View(model);
 				}
-
 			}
 
-			var currentUserId = Guid.Parse(User.GetId());
-			var currentFarmerId = await farmerService.GetFarmerIdByUserIdAsync(currentUserId);
-			var newFarmId = string.Empty;
+			//create new farm for current farmer
+			var newFarm = await farmService.AddNewFarmAsync(model, currentFarmerId);
+			var newFarmId = newFarm;
 
-			if (currentFarmerId != null)
-			{
-				var newFarm = await farmService.AddNewFarmAsync(model, currentFarmerId.Value);
-				newFarmId = newFarm.Id.ToString();
-			}
-
+			//return farm details view to see newly added farm
 			return RedirectToAction(nameof(Details), new { farmId = newFarmId });
 		}
 
 
 		[HttpGet]
-		public async Task<IActionResult> Edit(Guid farmId)
+		public async Task<IActionResult> Edit(string farmId)
 		{
 			//get current user and check if he's a farmer
-			var currentUserId = Guid.Parse(User.GetId());
+			var currentUserId = User.GetId();
 			var currentFarmerId = await farmerService.GetFarmerIdByUserIdAsync(currentUserId);
 
-			if (currentFarmerId == null)
+			//check if user is a farmer, if not redirect him to become one
+			if (string.IsNullOrEmpty(currentFarmerId))
+			{
+				return RedirectToAction("Become", "Farmer");
+			}
+
+			//get a list of all farm Id's of this farmer 
+			var farmerFarms = await farmService.GetOnlyFarmIdsByFarmerId(currentFarmerId);
+
+			//check if farmer has any farms and if currently edited farm has employed current farmer
+			if (farmerFarms != null && farmerFarms.All(id => id.ToLower() != farmId.ToLower()))
 			{
 				return RedirectToAction(nameof(Index));
 			}
 
-			//check if current edited farm is property of farmer
-			var farmerFarms = await farmService.GetFarmIdsByFarmerId(currentFarmerId.Value);
-			if (farmerFarms.All(f => f.ToLower() != farmId.ToString().ToLower()))
-			{
-				return RedirectToAction(nameof(Index));
-			}
-
-			//get tracked entity of farm to edit via services
+			//get tracked entity of farm to edit
 			var farmToEdit = await farmService.GetFarmToEditByIdAsync(farmId);
 
 			//if farm not found, redirect to all farms
@@ -121,8 +145,27 @@ namespace FarmersMarketApp.Web.Controllers
 		}
 
 		[HttpPost]
+		//todo: add anti-forgery token here
 		public async Task<IActionResult> Edit(AddFarmViewModel model)
 		{
+			var currentUserId = User.GetId();
+			var currentFarmerId = await farmerService.GetFarmerIdByUserIdAsync(currentUserId);
+
+			//ensure current user is a farmer, otherwise redirect him to become one
+			if (string.IsNullOrEmpty(currentFarmerId))
+			{
+				return RedirectToAction("Become", "Farmer");
+			}
+
+			//get a list of all farm Id's of this farmer 
+			var farmerFarms = await farmService.GetOnlyFarmIdsByFarmerId(currentFarmerId);
+
+			//check if farmer has any farms and if currently edited farm has employed current farmer
+			if (farmerFarms != null && farmerFarms.All(id => id.ToLower() != model.Id.ToLower()))
+			{
+				return RedirectToAction(nameof(Index));
+			}
+
 			//if model is invalid, return
 			if (!ModelState.IsValid)
 			{
@@ -161,8 +204,9 @@ namespace FarmersMarketApp.Web.Controllers
 			return RedirectToAction(nameof(Details), new { farmId = model.Id });
 		}
 
+		[AllowAnonymous]
 		[HttpGet]
-		public async Task<IActionResult> Details(Guid farmId)
+		public async Task<IActionResult> Details(string farmId)
 		{
 			var model = await farmService.GetFarmByIdReadOnlyAsync(farmId);
 
