4e67263f3aef05906c306acdb6fb383f39b4e09f
ezopus <133430790+ezopus@users.noreply.github.com>
2024-11-14 01:59:59 +0200

    added CreateNewPayment service

--- FarmersMarketApp.Services/Contracts/IPaymentService.cs
+++ FarmersMarketApp.Services/Contracts/IPaymentService.cs
@@ -1,6 +1,10 @@
-﻿namespace FarmersMarketApp.Services.Contracts
+﻿using FarmersMarketApp.Common.Enums;
+using FarmersMarketApp.Infrastructure.Data.Models;
+
+namespace FarmersMarketApp.Services.Contracts
 {
 	public interface IPaymentService
 	{
+		Task<string?> CreateNewPayment(string customerId, string orderId, PaymentType paymentType, decimal paymentAmount, Order order);
 	}
 }
--- FarmersMarketApp.Services/PaymentService.cs
+++ FarmersMarketApp.Services/PaymentService.cs
@@ -1,8 +1,37 @@
-﻿using FarmersMarketApp.Services.Contracts;
+﻿using FarmersMarketApp.Common.Enums;
+using FarmersMarketApp.Infrastructure.Data.Models;
+using FarmersMarketApp.Infrastructure.Repositories.Contracts;
+using FarmersMarketApp.Services.Contracts;
 
 namespace FarmersMarketApp.Services
 {
 	public class PaymentService : IPaymentService
 	{
+		private IRepository repository;
+		private readonly IUserService userService;
+
+		public PaymentService(
+			IRepository repository,
+			IUserService userService)
+		{
+			this.repository = repository;
+			this.userService = userService;
+		}
+		public async Task<string?> CreateNewPayment(string customerId, string orderId, PaymentType paymentType, decimal paymentAmount, Order order)
+		{
+			var newPayment = new Payment()
+			{
+				Id = Guid.NewGuid(),
+				CustomerId = Guid.Parse(customerId),
+				PaymentAmount = paymentAmount,
+				PaymentDate = DateTime.Now,
+				PaymentType = paymentType,
+				Order = order,
+			};
+
+			await repository.SaveChangesAsync();
+
+			return newPayment.Id.ToString();
+		}
 	}
 }
