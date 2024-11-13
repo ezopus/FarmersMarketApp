4680dcd69badab720e6543d70ce1d50c083f3996
ezopus <133430790+ezopus@users.noreply.github.com>
2024-11-14 01:58:46 +0200

    added Pending orders table in Order view

--- FarmersMarketApp.Web/wwwroot/js/ordersTableSwitcher.js
+++ FarmersMarketApp.Web/wwwroot/js/ordersTableSwitcher.js
@@ -3,20 +3,24 @@ let orderBtnArrow = document.getElementById('btnGroupDrop')
 let openOrdersTable = document.getElementById('openOrdersTable')
 let completedOrdersTable = document.getElementById('completedOrdersTable')
 let cancelledOrdersTable = document.getElementById('cancelledOrdersTable')
+let pendingOrdersTable = document.getElementById('pendingOrdersTable')
 
 
 document.getElementById('allOrdersBtn').addEventListener('click', () => {
     openOrdersTable.classList.remove('visually-hidden')
     completedOrdersTable.classList.remove('visually-hidden')
     cancelledOrdersTable.classList.remove('visually-hidden')
+    pendingOrdersTable.classList.remove('visually-hidden')
 
     orderBtn.classList.remove('btn-info')
     orderBtn.classList.remove('btn-warning')
+    orderBtn.classList.remove('btn-danger')
     orderBtn.classList.remove('btn-success')
     orderBtn.classList.add('btn-primary')
 
     orderBtnArrow.classList.remove('btn-info')
     orderBtnArrow.classList.remove('btn-warning')
+    orderBtnArrow.classList.remove('btn-danger')
     orderBtnArrow.classList.remove('btn-success')
     orderBtnArrow.classList.add('btn-primary')
 
@@ -25,17 +29,20 @@ document.getElementById('allOrdersBtn').addEventListener('click', () => {
 
 document.getElementById('openOrdersBtn').addEventListener('click', () => {
     openOrdersTable.classList.remove('visually-hidden')
+    pendingOrdersTable.classList.remove('visually-hidden')
     completedOrdersTable.classList.add('visually-hidden')
     cancelledOrdersTable.classList.add('visually-hidden')
 
     orderBtn.classList.remove('btn-primary')
     orderBtn.classList.remove('btn-success')
     orderBtn.classList.remove('btn-warning')
+    orderBtn.classList.remove('btn-danger')
     orderBtn.classList.add('btn-info')
 
     orderBtnArrow.classList.remove('btn-primary')
     orderBtnArrow.classList.remove('btn-success')
     orderBtnArrow.classList.remove('btn-warning')
+    orderBtnArrow.classList.remove('btn-danger')
     orderBtnArrow.classList.add('btn-info')
 
     orderBtn.textContent = 'Current order'
@@ -44,16 +51,19 @@ document.getElementById('openOrdersBtn').addEventListener('click', () => {
 document.getElementById('completedOrderBtn').addEventListener('click', () => {
     openOrdersTable.classList.add('visually-hidden')
     completedOrdersTable.classList.remove('visually-hidden')
-    cancelledOrdersTable.classList.add('visually-hidden')
+    completedOrdersTable.classList.add('visually-hidden')
+    pendingOrdersTable.classList.add('visually-hidden')
 
     orderBtn.classList.remove('btn-primary')
     orderBtn.classList.remove('btn-info')
     orderBtn.classList.remove('btn-warning')
+    orderBtn.classList.remove('btn-danger')
     orderBtn.classList.add('btn-success')
 
     orderBtnArrow.classList.remove('btn-primary')
     orderBtnArrow.classList.remove('btn-info')
     orderBtnArrow.classList.remove('btn-warning')
+    orderBtnArrow.classList.remove('btn-danger')
     orderBtnArrow.classList.add('btn-success')
 
     orderBtn.textContent = 'Complete orders'
@@ -62,17 +72,21 @@ document.getElementById('completedOrderBtn').addEventListener('click', () => {
 document.getElementById('cancelledOrdersBtn').addEventListener('click', () => {
     openOrdersTable.classList.add('visually-hidden')
     completedOrdersTable.classList.add('visually-hidden')
+    pendingOrdersTable.classList.add('visually-hidden')
     cancelledOrdersTable.classList.remove('visually-hidden')
 
     orderBtn.classList.remove('btn-primary')
     orderBtn.classList.remove('btn-success')
     orderBtn.classList.remove('btn-info')
-    orderBtn.classList.add('btn-warning')
+    orderBtn.classList.remove('btn-warning')
+    orderBtn.classList.add('btn-danger')
 
     orderBtnArrow.classList.remove('btn-primary')
     orderBtnArrow.classList.remove('btn-success')
     orderBtnArrow.classList.remove('btn-info')
-    orderBtnArrow.classList.add('btn-warning')
+    orderBtnArrow.classList.remove('btn-warning')
+    orderBtnArrow.classList.add('btn-danger')
 
     orderBtn.textContent = 'Cancelled orders'
 })
+
