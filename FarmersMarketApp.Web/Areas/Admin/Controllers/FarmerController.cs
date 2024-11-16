597ca818a26c3e550dc4003939cf1ea1e1a8bf6e
ezopus <133430790+ezopus@users.noreply.github.com>
2024-11-16 23:14:31 +0200

    added admin layout and farmer controller

---
+++ FarmersMarketApp.Web/Areas/Admin/Controllers/FarmerController.cs
@@ -0,0 +1,37 @@
+﻿using FarmersMarketApp.Services.Contracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FarmersMarketApp.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class FarmerController : Controller
+    {
+        private readonly IFarmerService farmerService;
+
+        public FarmerController(IFarmerService farmerService)
+        {
+            this.farmerService = farmerService;
+        }
+        public async Task<IActionResult> Manage()
+        {
+            var farmers = await farmerService.GetAllFarmersAsync();
+
+            return View(farmers);
+        }
+
+        public async Task<IActionResult> Restore(string farmerId)
+        {
+            var result = await farmerService.RestoreFarmerByIdAsync(farmerId);
+
+            return RedirectToAction(nameof(Manage));
+        }
+        public async Task<IActionResult> Delete(string farmerId)
+        {
+            var result = await farmerService.SetFarmerIsDeletedByIdAsync(farmerId);
+
+            return RedirectToAction(nameof(Manage));
+        }
+    }
+}
