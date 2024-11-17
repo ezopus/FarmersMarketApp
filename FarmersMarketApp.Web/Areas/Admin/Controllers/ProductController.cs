257d3b3527de0acdd9a1a34c99960dcc16152c49
ezopus <133430790+ezopus@users.noreply.github.com>
2024-11-17 15:18:07 +0200

    added restore and delete product functionality to admin product controller

---
+++ FarmersMarketApp.Web/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using FarmersMarketApp.Services.Contracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FarmersMarketApp.Web.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	[Authorize(Roles = "Admin")]
+	public class ProductController : Controller
+	{
+		private readonly IProductService productService;
+		public ProductController(IProductService productService)
+		{
+			this.productService = productService;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Manage()
+		{
+			//gets all products which are not deleted
+			var allModels = await productService.GetAllProductsAsync();
+
+			return View(allModels);
+		}
+
+		public async Task<IActionResult> Restore(string productId)
+		{
+			var result = await productService.RestoreProductByIdAsync(productId);
+
+			return RedirectToAction(nameof(Manage));
+		}
+		public async Task<IActionResult> Delete(string productId)
+		{
+			var result = await productService.SetProductIsDeletedByIdAsync(productId);
+
+			return RedirectToAction(nameof(Manage));
+		}
+
+	}
+}
