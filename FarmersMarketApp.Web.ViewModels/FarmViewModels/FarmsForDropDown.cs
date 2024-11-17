7072a076e709d3f0f6454529cfb9156a35eba103
ezopus <133430790+ezopus@users.noreply.github.com>
2024-11-17 19:59:52 +0200

    added category and farm selection drop downs

---
+++ FarmersMarketApp.Web.ViewModels/FarmViewModels/FarmsForDropDown.cs
@@ -0,0 +1,8 @@
+﻿namespace FarmersMarketApp.Web.ViewModels.FarmViewModels
+{
+	public class FarmsForDropDown
+	{
+		public string Id { get; set; } = null!;
+		public string Name { get; set; } = null!;
+	}
+}
