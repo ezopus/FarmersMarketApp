ed7fa39c006f55113dc4a8c43bbbcaf70a4e3243
ezopus <133430790+ezopus@users.noreply.github.com>
2024-11-17 15:16:07 +0200

    renamed service methods, added restore and delete all in admin farmer controller

--- FarmersMarketApp.Web/Areas/Admin/Controllers/FarmerController.cs
+++ FarmersMarketApp.Web/Areas/Admin/Controllers/FarmerController.cs
@@ -4,34 +4,46 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace FarmersMarketApp.Web.Areas.Admin.Controllers
 {
-    [Area("Admin")]
-    [Authorize(Roles = "Admin")]
-    public class FarmerController : Controller
-    {
-        private readonly IFarmerService farmerService;
+	[Area("Admin")]
+	[Authorize(Roles = "Admin")]
+	public class FarmerController : Controller
+	{
+		private readonly IFarmerService farmerService;
 
-        public FarmerController(IFarmerService farmerService)
-        {
-            this.farmerService = farmerService;
-        }
-        public async Task<IActionResult> Manage()
-        {
-            var farmers = await farmerService.GetAllFarmersAsync();
+		public FarmerController(IFarmerService farmerService)
+		{
+			this.farmerService = farmerService;
+		}
+		public async Task<IActionResult> Manage()
+		{
+			var farmers = await farmerService.GetAllFarmersAsync();
 
-            return View(farmers);
-        }
+			return View(farmers);
+		}
 
-        public async Task<IActionResult> Restore(string farmerId)
-        {
-            var result = await farmerService.RestoreFarmerByIdAsync(farmerId);
+		public async Task<IActionResult> Restore(string farmerId)
+		{
+			var result = await farmerService.RestoreFarmerByIdAsync(farmerId);
 
-            return RedirectToAction(nameof(Manage));
-        }
-        public async Task<IActionResult> Delete(string farmerId)
-        {
-            var result = await farmerService.SetFarmerIsDeletedByIdAsync(farmerId);
+			return RedirectToAction(nameof(Manage));
+		}
+		public async Task<IActionResult> RestoreAll(string farmerId)
+		{
+			var result = await farmerService.RestoreFarmerFarmsProductsByIdAsync(farmerId);
 
-            return RedirectToAction(nameof(Manage));
-        }
-    }
+			return RedirectToAction(nameof(Manage));
+		}
+		public async Task<IActionResult> Delete(string farmerId)
+		{
+			var result = await farmerService.SetFarmerIsDeletedByIdAsync(farmerId);
+
+			return RedirectToAction(nameof(Manage));
+		}
+		public async Task<IActionResult> DeleteAll(string farmerId)
+		{
+			var result = await farmerService.SetFarmerFarmsProductsIsDeletedByIdAsync(farmerId);
+
+			return RedirectToAction(nameof(Manage));
+		}
+	}
 }
--- FarmersMarketApp.Web/Controllers/FarmerController.cs
+++ FarmersMarketApp.Web/Controllers/FarmerController.cs
@@ -7,152 +7,152 @@ using static FarmersMarketApp.Common.DataValidation.ErrorMessages;
 
 namespace FarmersMarketApp.Web.Controllers
 {
-    public class FarmerController : BaseController
-    {
-        private readonly IFarmService farmService;
-        private readonly IFarmerService farmerService;
-        private readonly IUserService userService;
-        private readonly IProductService productService;
+	public class FarmerController : BaseController
+	{
+		private readonly IFarmService farmService;
+		private readonly IFarmerService farmerService;
+		private readonly IUserService userService;
+		private readonly IProductService productService;
 
-        public FarmerController(
-            IUserService userService,
-            IFarmerService farmerService,
-            IFarmService farmService,
-            IProductService productService)
-        {
-            this.farmerService = farmerService;
-            this.userService = userService;
-            this.farmService = farmService;
-            this.productService = productService;
-        }
+		public FarmerController(
+			IUserService userService,
+			IFarmerService farmerService,
+			IFarmService farmService,
+			IProductService productService)
+		{
+			this.farmerService = farmerService;
+			this.userService = userService;
+			this.farmService = farmService;
+			this.productService = productService;
+		}
 
-        //get all farmers in the market
-        [AllowAnonymous]
-        [HttpGet]
-        public async Task<IActionResult> Index()
-        {
-            var farmers = await farmerService.GetAllActiveFarmersAsync();
+		//get all farmers in the market
+		[AllowAnonymous]
+		[HttpGet]
+		public async Task<IActionResult> Index()
+		{
+			var farmers = await farmerService.GetAllActiveFarmersAsync();
 
-            return View(farmers);
-        }
+			return View(farmers);
+		}
 
-        [HttpGet]
-        public async Task<IActionResult> Become()
-        {
-            //get current user id
-            var currentUserId = User.GetId();
+		[HttpGet]
+		public async Task<IActionResult> Become()
+		{
+			//get current user id
+			var currentUserId = User.GetId();
 
-            //check if user is logged in
-            if (string.IsNullOrEmpty(currentUserId))
-            {
-                return RedirectToPage("/Account/Login", new { area = "Identity" });
-            }
+			//check if user is logged in
+			if (string.IsNullOrEmpty(currentUserId))
+			{
+				return RedirectToPage("/Account/Login", new { area = "Identity" });
+			}
 
-            var isFarmer = await userService.IsUserFarmerAsync(currentUserId);
+			var isFarmer = await userService.IsUserFarmerAsync(currentUserId);
 
-            //if user is already a farmer redirect to my farms
-            if (isFarmer)
-            {
-                return RedirectToAction(nameof(MyFarms), "Farmer");
-            }
+			//if user is already a farmer redirect to my farms
+			if (isFarmer)
+			{
+				return RedirectToAction(nameof(MyFarms), "Farmer");
+			}
 
-            return View();
-        }
+			return View();
+		}
 
-        [HttpPost]
-        [AutoValidateAntiforgeryToken]
-        //todo: add anti-forgery token here
-        public async Task<IActionResult> Become(FarmerBecomeViewModel model)
-        {
-            //get current user id
-            var currentUserId = User.GetId();
-            var currentFarmerId = await farmerService.GetFarmerIdByUserIdAsync(currentUserId);
+		[HttpPost]
+		[AutoValidateAntiforgeryToken]
+		//todo: add anti-forgery token here
+		public async Task<IActionResult> Become(FarmerBecomeViewModel model)
+		{
+			//get current user id
+			var currentUserId = User.GetId();
+			var currentFarmerId = await farmerService.GetFarmerIdByUserIdAsync(currentUserId);
 
-            //check if user is farmer, if he already is, redirect him to his farms
-            if (!string.IsNullOrEmpty(currentFarmerId))
-            {
-                return RedirectToAction("MyFarms", "Farmer");
-            }
+			//check if user is farmer, if he already is, redirect him to his farms
+			if (!string.IsNullOrEmpty(currentFarmerId))
+			{
+				return RedirectToAction("MyFarms", "Farmer");
+			}
 
-            //check model state
-            if (!ModelState.IsValid)
-            {
-                return View(model);
-            }
+			//check model state
+			if (!ModelState.IsValid)
+			{
+				return View(model);
+			}
 
-            //check if accept deliveries is pressed and set error if not
-            //workaround to not have any button selected when initial loading of page
-            if (model.AcceptsDeliveries == null)
-            {
-                ModelState.AddModelError(nameof(model.AcceptsDeliveries), ErrorAcceptDeliveriesNotSelected);
-                return View(model);
-            }
+			//check if accept deliveries is pressed and set error if not
+			//workaround to not have any button selected when initial loading of page
+			if (model.AcceptsDeliveries == null)
+			{
+				ModelState.AddModelError(nameof(model.AcceptsDeliveries), ErrorAcceptDeliveriesNotSelected);
+				return View(model);
+			}
 
-            //async create new farmer entity
-            var result = await farmerService.BecomeFarmerAsync(currentUserId, model);
+			//async create new farmer entity
+			var result = await farmerService.BecomeFarmerAsync(currentUserId, model);
 
-            if (string.IsNullOrEmpty(result))
-            {
-                return View(model);
-            }
+			if (string.IsNullOrEmpty(result))
+			{
+				return View(model);
+			}
 
-            //redirect to my farms
-            return RedirectToAction(nameof(MyFarms));
-        }
+			//redirect to my farms
+			return RedirectToAction(nameof(MyFarms));
+		}
 
-        [HttpGet]
-        //[MustBeFarmer]
-        public async Task<IActionResult> MyFarms()
-        {
-            //get current user id
-            var currentUserId = User.GetId();
-            var currentFarmerId = await farmerService.GetFarmerIdByUserIdAsync(currentUserId);
+		[HttpGet]
+		//[MustBeFarmer]
+		public async Task<IActionResult> MyFarms()
+		{
+			//get current user id
+			var currentUserId = User.GetId();
+			var currentFarmerId = await farmerService.GetFarmerIdByUserIdAsync(currentUserId);
 
-            //check if farmer exists, redirect to become one if not
-            if (string.IsNullOrEmpty(currentFarmerId))
-            {
-                return RedirectToAction("Become", "Farmer");
-            }
+			//check if farmer exists, redirect to become one if not
+			if (string.IsNullOrEmpty(currentFarmerId))
+			{
+				return RedirectToAction("Become", "Farmer");
+			}
 
-            //get all farms for current farmer
-            var farms = await farmService.GetAllFarmsByFarmerIdAsync(currentFarmerId);
+			//get all farms for current farmer
+			var farms = await farmService.GetAllFarmsByFarmerIdAsync(currentFarmerId);
 
-            //check if farms list is empty, if yes redirect to add farm
-            if (farms == null || !farms.Any())
-            {
-                return RedirectToAction("Add", "Farm");
-            }
+			//check if farms list is empty, if yes redirect to add farm
+			if (farms == null || !farms.Any())
+			{
+				return RedirectToAction("Add", "Farm");
+			}
 
-            //return model with all farms of current farmer
-            return View(farms);
-        }
+			//return model with all farms of current farmer
+			return View(farms);
+		}
 
-        [HttpGet]
-        //[MustBeFarmer]
-        public async Task<IActionResult> MyProducts()
-        {
-            //get current user id
-            var currentUserId = User.GetId();
-            var currentFarmerId = await farmerService.GetFarmerIdByUserIdAsync(currentUserId);
+		[HttpGet]
+		//[MustBeFarmer]
+		public async Task<IActionResult> MyProducts()
+		{
+			//get current user id
+			var currentUserId = User.GetId();
+			var currentFarmerId = await farmerService.GetFarmerIdByUserIdAsync(currentUserId);
 
-            //check if farmer exists, redirect to become one if not
-            if (string.IsNullOrEmpty(currentFarmerId))
-            {
-                return RedirectToAction("Become", "Farmer");
-            }
+			//check if farmer exists, redirect to become one if not
+			if (string.IsNullOrEmpty(currentFarmerId))
+			{
+				return RedirectToAction("Become", "Farmer");
+			}
 
-            //get all products for current farmer
-            var products = await productService.GetProductsByFarmerIdAsync(currentFarmerId);
+			//get all products for current farmer
+			var products = await productService.GetActiveProductsByFarmerIdAsync(currentFarmerId);
 
-            //check if products list is empty, if yes redirect to add product
-            if (products == null || !products.Any())
-            {
-                return RedirectToAction("Add", "Product");
-            }
+			//check if products list is empty, if yes redirect to add product
+			if (products == null || !products.Any())
+			{
+				return RedirectToAction("Add", "Product");
+			}
 
-            //return model with all farms to render view
-            return View(products);
-        }
+			//return model with all farms to render view
+			return View(products);
+		}
 
-    }
+	}
 }
