3f391335c962d6a9796d1248f0502a263b971776
ezopus <133430790+ezopus@users.noreply.github.com>
2024-11-19 23:31:36 +0200

    commented out some code to make email confirmation workable even though fake

--- FarmersMarketApp.Web/Areas/Identity/Pages/Account/Manage/Email.cshtml.cs
+++ FarmersMarketApp.Web/Areas/Identity/Pages/Account/Manage/Email.cshtml.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
+using System.Text.Encodings.Web;
 
 namespace FarmersMarketApp.Web.Areas.Identity.Pages.Account.Manage
 {
@@ -115,17 +116,17 @@ namespace FarmersMarketApp.Web.Areas.Identity.Pages.Account.Manage
 				user.Email = email;
 				//var code = await _userManager.GenerateChangeEmailTokenAsync(user, Input.NewEmail);
 				//code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-				//var callbackUrl = Url.Page(
-				//	"/Account/ConfirmEmailChange",
-				//	pageHandler: null,
-				//	values: new { area = "Identity", userId = userId, email = Input.NewEmail, code = code },
-				//	protocol: Request.Scheme);
-				//await _emailSender.SendEmailAsync(
-				//	Input.NewEmail,
-				//	"Confirm your email",
-				//	$"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+				var callbackUrl = Url.Page(
+					"/Account/ConfirmEmailChange",
+					pageHandler: null,
+					values: new { area = "Identity", userId = userId, email = Input.NewEmail, code = "code" },
+					protocol: Request.Scheme);
+				await _emailSender.SendEmailAsync(
+					Input.NewEmail,
+					"Confirm your email",
+					$"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
-				//StatusMessage = "Confirmation link to change email sent. Please check your email.";
+				StatusMessage = "Confirmation link to change email sent. Please check your email.";
 				return RedirectToPage();
 			}
 
@@ -147,8 +148,8 @@ namespace FarmersMarketApp.Web.Areas.Identity.Pages.Account.Manage
 				return Page();
 			}
 
-			//var userId = await _userManager.GetUserIdAsync(user);
-			//var email = await _userManager.GetEmailAsync(user);
+			var userId = await _userManager.GetUserIdAsync(user);
+			var email = await _userManager.GetEmailAsync(user);
 			//var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
 			//code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
 			//var callbackUrl = Url.Page(
@@ -161,7 +162,7 @@ namespace FarmersMarketApp.Web.Areas.Identity.Pages.Account.Manage
 			//	"Confirm your email",
 			//	$"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
-			//StatusMessage = "Verification email sent. Please check your email.";
+			StatusMessage = "Verification email sent. Please check your email.";
 			return RedirectToPage();
 		}
 	}
