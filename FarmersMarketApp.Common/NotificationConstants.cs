999e88b7e66de0888bf498ed899dc485682b37b9
ezopus <133430790+ezopus@users.noreply.github.com>
2024-11-26 23:06:25 +0200

    notification messages added to constants

--- FarmersMarketApp.Common/NotificationConstants.cs
+++ FarmersMarketApp.Common/NotificationConstants.cs
@@ -6,5 +6,20 @@
 		public const string ErrorMessage = "Error";
 		public const string InfoMessage = "Info";
 		public const string WarningMessage = "Warning";
+
+
+		public const string SuccessfullyApproveFarmer = "Successfully approved farmer!";
+		public const string FailedApproveFarmer = "Failed to approve farmer!";
+
+		public const string SuccessfullyRestoreFarmer = "Successfully restored farmer!";
+		public const string SuccessfullyRestoreFarmerEntities = "Successfully restored farmer and all his farms and products!";
+		public const string FailedRestoreFarmer = "Failed to restore farmer!";
+
+		public const string SuccessfullyDeleteFarmer = "Successfully deleted farmer!";
+		public const string SuccessfullyDeleteFarmerEntities = "Successfully deleted farmer and his entities!";
+		public const string FailedDeleteFarmer = "Failed to delete farmer!";
+
+		public const string AlreadyFarmer = "You are already a farmer.";
+		public const string SuccessfullyApplyToBeFarmer = "You have successfully applied to become a farmer!";
 	}
 }
