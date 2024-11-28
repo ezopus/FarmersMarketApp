306994f03232423090ceb7c0cdb26252696df8ed
ezopus <133430790+ezopus@users.noreply.github.com>
2024-11-29 00:30:08 +0200

    fixed some wrong titles and added blank asp-area on some buttons to avoid crashing when inside an area

--- FarmersMarketApp.Web/Controllers/ErrorController.cs
+++ FarmersMarketApp.Web/Controllers/ErrorController.cs
@@ -2,21 +2,22 @@
 
 namespace FarmersMarketApp.Web.Controllers
 {
-    public class ErrorController : Controller
-    {
-        [Route("Error/{statusCode}")]
-        public IActionResult HttpStatusCodeHandler(int statusCode)
-        {
-            var viewName = statusCode switch
-            {
-                403 => "Unauthorized",
-                404 => "NotFound",
-                405 => "NotApproved",
-                500 => "ServerError",
-                _ => "Error"
-            };
+	public class ErrorController : Controller
+	{
+		[Route("Error/{statusCode}")]
+		public IActionResult HttpStatusCodeHandler(int statusCode)
+		{
+			var viewName = statusCode switch
+			{
+				401 => "Unauthorized",
+				403 => "Unauthorized",
+				404 => "NotFound",
+				405 => "NotApproved",
+				500 => "ServerError",
+				_ => "Error"
+			};
 
-            return View(viewName);
-        }
-    }
+			return View(viewName);
+		}
+	}
 }
