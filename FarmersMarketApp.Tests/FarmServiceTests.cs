4a8a68f822fbed76d350e56c1753abf57550211f
ezopus <133430790+ezopus@users.noreply.github.com>
2024-12-01 22:51:19 +0200

    startet farm service tests

---
+++ FarmersMarketApp.Tests/FarmServiceTests.cs
@@ -0,0 +1,39 @@
+﻿using FarmersMarketApp.Infrastructure.Repositories;
+using FarmersMarketApp.Infrastructure.Repositories.Contracts;
+using FarmersMarketApp.Services;
+using FarmersMarketApp.Services.Contracts;
+
+namespace FarmersMarketApp.Tests
+{
+	[TestFixture]
+	public class FarmServiceTests : BaseUnitTests
+	{
+		private IRepository repository;
+		private IProductService productService;
+		private IFarmService farmService;
+
+		[OneTimeSetUp]
+		public void Setup()
+		{
+			repository = new FarmersMarketRepository(contextMock);
+			productService = new ProductService(repository);
+			farmService = new FarmService(repository, productService);
+		}
+
+		[Test]
+		public async Task GetAllFarmsAsync_ShouldReturnAllFarms()
+		{
+			// Arrange & Act
+			var result = await farmService.GetAllFarmsAsync();
+
+			// Assert
+			Assert.That(result, Is.Not.Null);
+			Assert.That(result.Count(), Is.EqualTo(2));
+			Assert.That(result.First().Name, Is.EqualTo("Farm1"));
+		}
+
+
+
+	}
+
+}
