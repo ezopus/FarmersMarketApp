85aa5b9a090431f99bba3ff918ad33a9aec0c24f
ezopus <133430790+ezopus@users.noreply.github.com>
2024-12-01 22:50:36 +0200

    added Mock database and TestApplicationDbContext for unit tests

---
+++ FarmersMarketApp.Tests/Mocks/DatabaseMock.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FarmersMarketApp.Tests.Mocks
+{
+	public static class DatabaseMock
+	{
+		public static TestApplicationDbContext ContextInstance
+		{
+			get
+			{
+				var dbContextOptions = new DbContextOptionsBuilder<TestApplicationDbContext>()
+					.UseInMemoryDatabase("FarmersMarketInMemoryDb" + DateTime.Now.Ticks.ToString())
+					.Options;
+
+				return new TestApplicationDbContext(dbContextOptions);
+			}
+
+		}
+	}
+}
