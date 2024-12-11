d83b56a7555ef0392a9439a74781e04412caa9a1
ezopus <133430790+ezopus@users.noreply.github.com>
2024-12-11 22:14:06 +0200

    added table names to order tables

--- FarmersMarketApp.ViewModels/OrderViewModels/ManageFarmerOrdersViewModel.cs
+++ FarmersMarketApp.ViewModels/OrderViewModels/ManageFarmerOrdersViewModel.cs
@@ -11,5 +11,7 @@ namespace FarmersMarketApp.ViewModels.OrderViewModels
 		public string TableId { get; set; } = string.Empty;
 
 		public string Classes { get; set; } = string.Empty;
+
+		public string TableName { get; set; } = string.Empty;
 	}
 }
--- FarmersMarketApp.ViewModels/OrderViewModels/ManageUserOrdersViewModel.cs
+++ FarmersMarketApp.ViewModels/OrderViewModels/ManageUserOrdersViewModel.cs
@@ -9,5 +9,7 @@
 		public string TableId { get; set; } = string.Empty;
 
 		public string Classes { get; set; } = string.Empty;
+
+		public string TableName { get; set; } = string.Empty;
 	}
 }
